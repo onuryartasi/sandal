a6ae4a7dc73833d366db048e090084180108e309
Onur YARTAŞI <onuryartasi@live.com>
2019-02-09 13:26:11 +0300

    Request client test

---
+++ pkg/request/request_test.go
@@ -0,0 +1,18 @@
+package request
+
+import (
+	"testing"
+	"net/http"
+)
+
+func TestDockerSocket(t *testing.T){
+	client := NewClient()
+	response, err := client.Get("http://v1.37/_ping")
+	if err != nil {
+		t.Logf("Docker Socket Error %v",err)
+	}
+
+	if response.StatusCode != http.StatusOK{
+		t.Logf("Docker socket not running, %v",err)
+	}
+}
