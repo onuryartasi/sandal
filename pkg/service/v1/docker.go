ed8dc880b28777e47eba33d91dbf50f9ce6a72f0
Onur Yartaşı <onuryartasi@live.com>
2019-02-11 19:55:35 +0300

    TestService_ContainerList added

--- pkg/service/v1/docker.go
+++ pkg/service/v1/docker.go
@@ -1,21 +1,24 @@
 package v1
 
 import (
-	"os"
-	"github.com/docker/docker/client"
-	"log"
-	"github.com/docker/docker/api/types"
 	"context"
+	"log"
+	"os"
 
-	"github.com/onuryartasi/scaler/pkg/api/v1"
-	"github.com/golang/protobuf/ptypes/empty"
+	"github.com/docker/docker/api/types"
+	"github.com/docker/docker/client"
+
 	"time"
+
 	"github.com/docker/docker/api/types/container"
+	"github.com/golang/protobuf/ptypes/empty"
+	v1 "github.com/onuryartasi/scaler/pkg/api/v1"
 
 	"encoding/json"
-	"github.com/onuryartasi/scaler/pkg/metric"
-	"strconv"
 	"fmt"
+	"strconv"
+
+	"github.com/onuryartasi/scaler/pkg/metric"
 	p2 "github.com/onuryartasi/scaler/pkg/types"
 )
 
@@ -28,57 +31,56 @@ var (
 )
 
 type Service struct{}
-type Error struct {}
-
+type Error struct{}
 
 var projects []p2.Project
 var cli *client.Client
+
 func init() {
 	var err error
-	if len(os.Getenv("DOCKER_API_VERSION")) < 1{
+	if len(os.Getenv("DOCKER_API_VERSION")) < 1 {
 		os.Setenv("DOCKER_API_VERSION", "1.37")
 	}
 
-	cli,err = client.NewClientWithOpts(client.FromEnv)
+	cli, err = client.NewClientWithOpts(client.FromEnv)
 	if err != nil {
-		log.Fatalf(ErrorColor,"Error: %s",err)
+		log.Fatalf(ErrorColor, "Error: %s", err)
 	}
 }
 
-var timeout time.Duration = 10*time.Second
-func (s *Service) ContainerList(ctx context.Context,empty *empty.Empty) (*v1.Containers, error){
+var timeout time.Duration = 10 * time.Second
+
+func (s *Service) ContainerList(ctx context.Context, empty *empty.Empty) (*v1.Containers, error) {
 	containers, err := cli.ContainerList(context.Background(), types.ContainerListOptions{})
 	if err != nil {
-		log.Fatalf(ErrorColor,"Error: %s",err)
+		log.Fatalf(ErrorColor, "Error: %s", err)
 	}
 
 	rContainers := []*v1.Container{}
-	for _,container := range containers{
-		rContainers = append(rContainers,&v1.Container{Names:container.Names,Id:container.ID,Image:container.Image})
+	for _, container := range containers {
+		rContainers = append(rContainers, &v1.Container{Names: container.Names, Id: container.ID, Image: container.Image})
 	}
-	return &v1.Containers{Container:rContainers},nil
+	return &v1.Containers{Container: rContainers}, nil
 }
 
+func (s *Service) ContainerStop(ctx context.Context, containerId *v1.ContainerId) (*v1.ContainerId, error) {
 
-func (s *Service) ContainerStop(ctx context.Context,containerId *v1.ContainerId) (*v1.ContainerId,error){
-
-
-	err := cli.ContainerStop(ctx,containerId.GetContainerId(),&timeout)
-	return containerId,err
+	err := cli.ContainerStop(ctx, containerId.GetContainerId(), &timeout)
+	return containerId, err
 }
 
-func (s *Service) ContainerStart(ctx context.Context,containerId *v1.ContainerId)(*v1.ContainerId,error){
+func (s *Service) ContainerStart(ctx context.Context, containerId *v1.ContainerId) (*v1.ContainerId, error) {
 
-	err := cli.ContainerStart(ctx,containerId.GetContainerId(),types.ContainerStartOptions{})
-	return containerId,err
+	err := cli.ContainerStart(ctx, containerId.GetContainerId(), types.ContainerStartOptions{})
+	return containerId, err
 }
 
-func (s *Service) ContainerCreate(ctx context.Context,config *v1.ContainerConfig)(*v1.Container,error){
-	resp,err := cli.ContainerCreate(ctx,&container.Config{Image:config.GetImage()},nil,nil,"")
-	return &v1.Container{Id:resp.ID},err
+func (s *Service) ContainerCreate(ctx context.Context, config *v1.ContainerConfig) (*v1.Container, error) {
+	resp, err := cli.ContainerCreate(ctx, &container.Config{Image: config.GetImage()}, nil, nil, "")
+	return &v1.Container{Id: resp.ID}, err
 }
 
-func (s *Service) ContainerStatStream(containerId *v1.ContainerId,stream v1.ContainerService_ContainerStatStreamServer) error{
+func (s *Service) ContainerStatStream(containerId *v1.ContainerId, stream v1.ContainerService_ContainerStatStreamServer) error {
 	var err error
 	for {
 		response, err := cli.ContainerStats(context.Background(), containerId.GetContainerId(), true)
@@ -88,42 +90,42 @@ func (s *Service) ContainerStatStream(containerId *v1.ContainerId,stream v1.Cont
 		stat := p2.Metric{}
 		json.NewDecoder(response.Body).Decode(&stat)
 
-		stream.Send(&v1.ContainerStat{Name:stat.Name})
+		stream.Send(&v1.ContainerStat{Name: stat.Name})
 	}
 	return err
 }
 
-func (s *Service) CreateProject(ctx context.Context,project *v1.Project) (*v1.ProjectInfo,error) {
+func (s *Service) CreateProject(ctx context.Context, project *v1.Project) (*v1.ProjectInfo, error) {
 	image := project.GetImage()
 	max := int(project.GetMax())
 	min := int(project.GetMin())
 
-	tmp := p2.Project{Max:max,Min:min,Image:image,Name:project.GetName()}
+	tmp := p2.Project{Max: max, Min: min, Image: image, Name: project.GetName()}
 
 	var containers []string
-	for i:=0;i<min;i++{
+	for i := 0; i < min; i++ {
 
-		resp,err := cli.ContainerCreate(context.Background(),&container.Config{Image:image},nil,nil,fmt.Sprintf("%s%s",project.GetName(),strconv.Itoa(i+1)))
+		resp, err := cli.ContainerCreate(context.Background(), &container.Config{Image: image}, nil, nil, fmt.Sprintf("%s%s", project.GetName(), strconv.Itoa(i+1)))
 
 		if err != nil {
-			log.Println("[CREATE_PROJECT] Creating Container error: %v",err)
+			log.Printf("[CREATE_PROJECT] Creating Container error: %v", err)
 
 		}
-		err = cli.ContainerStart(ctx,resp.ID,types.ContainerStartOptions{})
-		if err != nil{
-			log.Printf("Contaner Not starting, %s",err)
+		err = cli.ContainerStart(ctx, resp.ID, types.ContainerStartOptions{})
+		if err != nil {
+			log.Printf("Contaner Not starting, %s", err)
 		}
-		containers = append(containers,string(resp.ID))
+		containers = append(containers, string(resp.ID))
 	}
 
 	tmp.Containers = containers
 	go metric.ProjectMetric(tmp)
-	projects = append(projects,tmp)
-	return &v1.ProjectInfo{ContainerId:containers},nil
+	projects = append(projects, tmp)
+	return &v1.ProjectInfo{ContainerId: containers}, nil
 
 }
 
-func GetProjects() *[]p2.Project{
+func GetProjects() *[]p2.Project {
 	return &projects
 }
 
--- pkg/service/v1/docker_test.go
+++ pkg/service/v1/docker_test.go
@@ -1,11 +1,20 @@
 package v1
 
 import (
+	"context"
+
+	"github.com/golang/protobuf/ptypes/empty"
+
 	"testing"
 )
 
-
-
 func TestService_ContainerList(t *testing.T) {
+	s := Service{}
+
+	req := &empty.Empty{}
+	_, err := s.ContainerList(context.Background(), req)
+	if err != nil {
+		t.Errorf("Error ContainerList")
+	}
 
 }
